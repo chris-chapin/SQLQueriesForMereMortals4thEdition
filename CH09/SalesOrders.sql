083a495247198c4718c9ccf563e5d0acaadf8b25
Chris Chapin <cchapin@outlook.com>
2022-06-10 16:51:09 -0700

    CH09 Solutions

--- CH09/SalesOrders.sql
+++ CH09/SalesOrders.sql
@@ -1,7 +1,29 @@
 -- "Show me customers who have never ordered a helmet."
 
-
+SELECT (Customers.CustFirstName + ' ' + Customers.CustLastName) AS Customer
+FROM Customers
+LEFT OUTER JOIN (
+	SELECT Orders.CustomerID, Orders.OrderNumber, Products.ProductName
+	FROM ((Orders
+		INNER JOIN Order_Details ON Order_Details.OrderNumber = Orders.OrderNumber)
+		INNER JOIN Products ON Products.ProductNumber = Order_Details.ProductNumber)
+	WHERE Products.ProductName LIKE '%Helmet%') AS HelmetOrders
+ON HelmetOrders.CustomerID = Customers.CustomerID
+WHERE HelmetOrders.ProductName is NULL
 
 -- "Display customers who have no sales rep in the same ZIP Code."
 
+SELECT (Customers.CustFirstName + ' ' + Customers.CustLastName) AS Customer
+FROM Customers
+LEFT OUTER JOIN Employees ON Employees.EmpZipCode = Customers.CustZipCode
+WHERE Employees.EmpZipCode is NULL
+
 -- "List all products and the dates for any orders."
+
+SELECT ProductName, OrdersDetails.OrderDate
+FROM Products
+LEFT OUTER JOIN (
+	SELECT DISTINCT Orders.OrderDate, Order_Details.ProductNumber
+	FROM Orders
+	INNER JOIN Order_Details ON Order_Details.OrderNumber = Orders.OrderNumber) AS OrdersDetails
+ON OrdersDetails.ProductNumber = Products.ProductNumber
--- CH09/SchoolSchedule.sql
+++ CH09/SchoolSchedule.sql
@@ -1,7 +1,38 @@
 -- "Show me classes that have no students enrolled."
 
+SELECT DISTINCT Classes.ClassID, Subjects.SubjectName
+FROM (Classes
+	LEFT OUTER JOIN (
+		SELECT Student_Schedules.ClassID, Student_Schedules.StudentID, Student_Class_Status.ClassStatusDescription
+		FROM Student_Schedules
+		INNER JOIN Student_Class_Status ON Student_Class_Status.ClassStatus = Student_Schedules.ClassStatus) AS StudentEnrolledmentStatus
+	ON StudentEnrolledmentStatus.ClassID = Classes.ClassID)
+INNER JOIN Subjects ON Subjects.SubjectID = Classes.SubjectID
+WHERE StudentEnrolledmentStatus.StudentID is NULL OR StudentEnrolledmentStatus.ClassStatusDescription <> 'enrolled'
+
 -- "Display subjects with no faculty assigned."
+SELECT Subjects.SubjectName
+FROM Subjects
+LEFT OUTER JOIN Faculty_Subjects ON Faculty_Subjects.SubjectID = Subjects.SubjectID
+WHERE Faculty_Subjects.StaffID is NULL
 
 -- "List students not currently enrolled in any classes."
 
+SELECT (Students.StudFirstName + ' ' + Students.StudLastName) AS StudentName
+FROM Students
+LEFT OUTER JOIN (
+	SELECT (Students.StudFirstName + ' ' + Students.StudLastName) AS EnrolledStudentName, Students.StudentID AS EnrolledStudentId
+	FROM Students
+	INNER JOIN Student_Schedules ON Student_Schedules.StudentID = Students.StudentID
+	WHERE Student_Schedules.ClassStatus = 1) AS EnrolledStudents
+ON EnrolledStudents.EnrolledStudentId = Students.StudentID
+WHERE EnrolledStudents.EnrolledStudentId is NULL
+
 -- "Display all faculty and the classes they are scheduled to teach."
+SELECT (Staff.StfFirstName + ' ' + Staff.StfLastname) AS StaffName, FacultyClasses.ClassID
+FROM Staff
+LEFT OUTER JOIN (
+	SELECT Classes.ClassID, Faculty_Classes.StaffID
+	FROM Faculty_Classes
+	INNER JOIN Classes ON Classes.ClassID = Faculty_Classes.ClassID) AS FacultyClasses
+ON FacultyClasses.StaffID = Staff.StaffID
